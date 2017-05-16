8b4daa16b5cff0ccc971d068b93676a7c7ba392f
Théo Szymkowiak <theoszymko@me.com>
2017-05-16 17:13:23 -0400

    Color

--- src/configParser.js
+++ src/configParser.js
@@ -5,7 +5,7 @@ module.exports = function() {
     var config = configFunction(process.env.NODE_ENV);
 
     if(!config.database) {
-        console.log(colors.orange('Warning: ') + colors.green('Database name') + ' is not set in config.js');
+        console.log(colors.red('Warning: ') + colors.green('Database name') + ' is not set in config.js');
     }
 
     if(!config.user) {
