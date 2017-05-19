d3371b2a913daf1e2ca2060b85b7e86fac69f33c
Théo Szymkowiak <theoszymko@me.com>
2017-05-19 13:43:49 -0400

    Removing database from config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 module.exports = function(environment) {
     return {
         connectionLimit: 20,
-        // Default database to execute queries on
-        database: process.env.MYSQL_DATABASE,
         user: process.env.MYSQL_USER,
         port: process.env.MYSQL_PORT,
         password: process.env.MYSQL_PASSWORD,
         host: process.env.MYSQL_HOST,
+        // Master secret token
         secretToken: process.env.SECRET_TOKEN,
         // Enable users, will create a lambdadb_config
         // database following the format in 
--- src/configParser.js
+++ src/configParser.js
@@ -4,10 +4,6 @@ var configFunction = require('./../config.js'),
 module.exports = function() {
     var config = configFunction(process.env.NODE_ENV);
 
-    if(!config.database) {
-        console.log(colors.red('Warning: ') + colors.green('Database name') + ' is not set in config.js');
-    }
-
     if(!config.user) {
         throw new Error('Database ' + colors.green('user') + ' must be set in config.js');
     }
